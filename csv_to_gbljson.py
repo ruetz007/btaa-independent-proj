910961d124299fe7d704aeba11362fd4062f788c
ruetzee <e.ruetz@comcast.net>
2019-07-14 20:42:29 -0500

    simple json with most required fields

--- csv_to_gbljson.py
+++ csv_to_gbljson.py
@@ -2,28 +2,26 @@ import csv
 import json
 import os
 
-os.mkdir("json")
+crosswalk_dict = { #dictionary to translate Dublin Core/GBL into GBLJson
+    "Dublin Core:Identifier":["layer_slug_s","dc_identifier_s"],
+    "Dublin Core:Provenance":["dct_provenance_s"],
+    "Dublin Core:Title":["dc_title_s"]
+    }
+
+if not os.path.exists("json"): #create a folder to store the jsons
+    os.mkdir("json")
+    
 csvfile = open('ArcGIS_Reaccession_20190607 - actualNew.csv', 'r')
-#jsonfile = open('file.json', 'w')
 
-crosswalk_dict = {"Dublin Core:Identifier":["layer_slug_s","dc_identifier_s"]}
-                  
-
-fieldnames = ("layer_slug_s","geoblacklight_version","dct_provenance_s","dc_title_s","dc_rights_s","dc_identifier_s")
 reader = csv.DictReader(csvfile)
 
 for row in reader: #row is a dictionary
-    #print row
+    small_dict = {"geoblacklight_version":"1.0","dc_rights_s":"Public"}
+    for key,val in row.items():
+        if key in crosswalk_dict:
+            for fieldname in crosswalk_dict[key]:
+                small_dict[fieldname] = val
     iden = row['Dublin Core:Identifier']
     filename = iden + ".json"
-    jsonfile = open("json/"+filename, 'w')
-    json.dump(row,jsonfile)
-    
-
-#filter_dict = {}
-#for key,val in reader.items():
-#    if key is in crosswalk_dict:
-#        filter_dict
-
-#out = json.dump( [ row for row in reader ], jsonfile )
-#jsonfile.write(out)
+    with open("json/"+filename, 'w') as jsonfile:
+        json.dump(small_dict,jsonfile)
