265c52a9252f265ec1e10d3fef1fc0cf4644cab7
karenmajewicz <majew030@umn.edu>
2019-08-08 12:47:42 -0500

    added new fields and fixed timestamp

--- csv_to_gbljson.py
+++ csv_to_gbljson.py
@@ -4,25 +4,38 @@ import os
 from datetime import datetime
 
 single_dict = { #dictionary to translate Dublin Core/GBL into GBLJson
-    "Dublin Core:Identifier":["layer_slug_s","dc_identifier_s"],
-    "Dublin Core:Provenance":["dct_provenance_s"],
-    "Dublin Core:Title":["dc_title_s"],
-    "Dublin Core:Date":["solr_year_i"],
-    "GeoBlacklight:Geometry Type":["layer_geom_type_s"],
-    "Dublin Core:Date Issued":["dct_issued_s"]
+    "Identifier":["layer_slug_s","dc_identifier_s"],
+    "Provenance":["dct_provenance_s"],
+    "Title":["dc_title_s"],
+    "Date":["solr_year_i"],
+    "Geometry Type":["layer_geom_type_s"],
+    "Date Issued":["dct_issued_s"],
+    "Code":["b1g_code_s"],
+    "Date Accepted":["dct_dateAccepted_s"],
+    "Status":["b1g_status_s"],
+    "Accrual Method":["dct_accrualMethod_s"],
+    "Description":["dc_description_s"],
+    "Format":["dc_format_s"],
+    "References":["dct_references_s"],
+    "Bounding Box":["solr_geom"],
+    "Centroid":["b1g_centroid_ss"]
     }
 multiple_dict = {
-    "Dublin Core:Spatial Coverage":["dct_spatial_sm"],
-    "Dublin Core:Temporal Coverage":["dct_temporal_sm"],
-    "Dublin Core:Is Part Of":["dct_isPartOf_sm"]
+    "Spatial Coverage":["dct_spatial_sm"],
+    "Temporal Coverage":["dct_temporal_sm"],
+    "Is Part Of":["dct_isPartOf_sm"],
+    "Genre":["b1g_genre_sm"],
+    "Subject":["dc_subject_sm"],
+    "Type":["dc_type_sm"],
+    "Creator":["dc_creator_sm"]
     }
 if not os.path.exists("json"): #create a folder to store the jsons
     os.mkdir("json")
-    
-csvfile = open('ArcGIS_Reaccession_20190607 - actualNew.csv', 'r')
+
+csvfile = open('Wi-20190807.csv', 'r')
 
 reader = csv.DictReader(csvfile)
-date_modified = datetime.today().strftime('%Y-%m-%d')
+date_modified = datetime.today().strftime('%Y-%m-%d')+"T"+datetime.today().strftime('%X')+"Z"
 
 for row in reader: #row is a dictionary
     small_dict = {"geoblacklight_version":"1.0","dc_rights_s":"Public","layer_modified_dt":date_modified}
@@ -33,7 +46,7 @@ for row in reader: #row is a dictionary
         if key in multiple_dict:
             for fieldname in multiple_dict[key]:
                 small_dict[fieldname] = val.split('|')
-    iden = row['Dublin Core:Identifier']
+    iden = row['Identifier']
     filename = iden + ".json"
     with open("json/"+filename, 'w') as jsonfile:
         json.dump(small_dict,jsonfile,indent=2)
