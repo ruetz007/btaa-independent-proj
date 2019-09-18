d835d2f1c35a152b72f856e54f0dc4062604fbd3
emilyruetz <emily.ruetz@state.mn.us>
2019-09-18 15:17:56 -0500

    added inline comments to code

--- csv_to_gbljson.py
+++ csv_to_gbljson.py
@@ -3,7 +3,7 @@ import json
 import os
 from datetime import datetime
 
-single_dict = { #dictionary to translate Dublin Core/GBL into GBLJson
+single_dict = { #dictionary to translate single-value Dublin Core/GBL fields into GBLJson
     "Dublin Core:Identifier":["layer_slug_s","dc_identifier_s"],
     "Dublin Core:Provenance":["dct_provenance_s"],
     "Dublin Core:Title":["dc_title_s"],
@@ -11,36 +11,36 @@ single_dict = { #dictionary to translate Dublin Core/GBL into GBLJson
     "GeoBlacklight:Geometry Type":["layer_geom_type_s"],
     "Dublin Core:Date Issued":["dct_issued_s"]
     }
-multiple_dict = {
+multiple_dict = { #dictionary to translate multivalue Dublin Core/GBL fields into GBLJson
     "Dublin Core:Spatial Coverage":["dct_spatial_sm"],
     "Dublin Core:Temporal Coverage":["dct_temporal_sm"],
     "Dublin Core:Is Part Of":["dct_isPartOf_sm"]
     }
-if not os.path.exists("json"): #create a folder to store the jsons
+if not os.path.exists("json"): #create a folder to store the jsons if one does not already exist
     os.mkdir("json")
     
-csvfile = open('ArcGIS_Reaccession_20190607 - actualNew.csv', 'r')
+csvfile = open('ArcGIS_Reaccession_20190607 - actualNew.csv', 'r') #opens the csv with the GBL data. Change this as needed
 
 reader = csv.DictReader(csvfile)
-date_modified = datetime.today().strftime('%Y-%m-%d')
+date_modified = datetime.today().strftime('%Y-%m-%d') #sets date modified to the current date
 
-for row in reader: #row is a dictionary
+for row in reader: #each row is a dictionary
     code = ""
-    small_dict = {"geoblacklight_version":"1.0","dc_rights_s":"Public","layer_modified_dt":date_modified}
+    small_dict = {"geoblacklight_version":"1.0","dc_rights_s":"Public","layer_modified_dt":date_modified} #starting dictionary with set values
     for key,val in row.items():
         if key == "Code":
             code = val
-            if not os.path.exists("json/" + val):
+            if not os.path.exists("json/" + val): #makes a new folder for each code
                 os.mkdir("json/" + val)
         if key in single_dict:
             for fieldname in single_dict[key]:
                 small_dict[fieldname] = val
         if key in multiple_dict:
             for fieldname in multiple_dict[key]:
-                small_dict[fieldname] = val.split('|')
+                small_dict[fieldname] = val.split('|') #creates a list with the multiple values
         if key == "Dublin Core:Coverage":
             val = val.split(',')
-            if len(val) == 4:
+            if len(val) == 4: #takes care of bounding box values and calculates centroid
                 west = val[0]
                 south = val[1]
                 east = val[2]
@@ -49,10 +49,10 @@ for row in reader: #row is a dictionary
                 centerlong = (float(east)+float(west))/2
                 small_dict["solr_geom"] = "ENVELOPE("+west+","+east+","+north+","+south+")"
                 small_dict["b1g_centroid_ss"] = str(centerlat) + "," + str(centerlong)
-            else:
+            else: #if the bounding box doesn't have all coordinates, just write values as null
                 small_dict["solr_geom"] = "NULL"
                 small_dict["b1g_centroid_ss"] = "NULL"
     iden = row['Dublin Core:Identifier']
     filename = iden + ".json"
-    with open("json/"+code+"/"+filename, 'w') as jsonfile:
+    with open("json/"+code+"/"+filename, 'w') as jsonfile: #writes to a json with the identifier as the filename
         json.dump(small_dict,jsonfile,indent=2)
