f33ea4b1d09583217932df8f736345db9a296a17
ruetzee <e.ruetz@comcast.net>
2019-11-24 19:51:46 -0600

    Fixed header ordering issue; all values fall under correct headers

--- json_to_csv.py
+++ json_to_csv.py
@@ -3,22 +3,22 @@ import json
 import os
 
 ### change this file path to point to the folder that contains all the jsons
-json_path = r"C:\Users\Emily\Documents\Grad School\GIS 5578\GitHub\class-project-ruetz007\Karen_test_Sept2019\Karen_test_Sept2019"
+json_path = r"C:\Users\Emily\Documents\Grad School\GIS 5578\GitHub\class-project-ruetz007\wi-NAIP"
 
 ### change this filename to be the desired name of the output csv. It will write into the directory that contains this Python script
-csv_out_file = "result.csv"
+csv_out_file = "resultWI.csv"
 
 #if not os.path.exists("csv"): #create a folder to store the csv if one does not already exist
 #    os.mkdir("csv")
 
 first = True
+headers = []
 f = csv.writer(open(csv_out_file, "w", newline=''))
 
 for filename in os.listdir(json_path):
     #print(filename)
     if filename.endswith(".json"):
         values = []
-        headers = []
         json_file_open = open(json_path+"\\"+filename, 'rb')
         data = json_file_open.read().decode('utf-8', errors='ignore')
         #print(data)
@@ -29,16 +29,20 @@ for filename in os.listdir(json_path):
                 headers.append(x)
             f.writerow(headers)
             first = False
-        for key in loaded:
-            multiple = []
-            if type(loaded[key]) == str:
-                values.append(loaded[key])
-            elif type(loaded[key]) == list:
-                for y in loaded[key]:
-                    multiple.append(y)
-                values.append("".join(multiple))
-            else:
-                print("something else")
+        for k in headers:
+            for key in loaded:
+                multiple = []
+                if key == k:
+                    if type(loaded[key]) == str:
+                        values.append(loaded[key])
+                    elif type(loaded[key]) == int or type(loaded[key]) == float:
+                        values.append(str(loaded[key]))
+                    elif type(loaded[key]) == list:
+                        for y in loaded[key]:
+                            multiple.append(y)
+                        values.append("".join(multiple))
+                    else:
+                        print("something else")
         f.writerow(values)
     json_file_open.close()
 #f.close()
